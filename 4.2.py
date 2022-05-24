56edf852df2664205efa852297e63f646719f71a
billygl <billy.grados@gmail.com>
2022-05-24 18:17:42 -0500

    fix: consider consecutive repeated numbers

--- 4.2.py
+++ 4.2.py
@@ -8,21 +8,31 @@ def genArray():
 
 def printArray(array):
     for i in range(0, len(array)):
-        print(array[i], end='\t')
+        print(array[i], end=' ')
     print('\n')
 
 def removeDuplicates(array):
+    print(array)
     l = len(array)
     r = array[:]
-    for i in range(0, l):
-        for j in range(i + 1, l):
-            print(i, j, r[i], r[j])
+    i = 0
+    while i < l:
+        j = i + 1
+        while j < l:
+            #print(i, ':', r[i], '-', j, ':', r[j] )
             if r[i] == r[j]:
-                for k in range(j + 1, l):
-                    r[k - 1] = r[k]
+                for k in range(j, l - 1):
+                    r[k] = r[k + 1]
                 r[l - 1] = 0
+                j = i + 1 #fix consecutive repeated numbers
+                print(r)
+            if r[i] == 0 and r[j] == 0:
+                break
+            j += 1
+        i = i + 1
     return r
 
+array = [4, 9, 3, 5, 3, 3, 10, 10, 5, 2] 
 array = genArray()
 printArray(array)
 array = removeDuplicates(array)
